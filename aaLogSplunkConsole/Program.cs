ae8fb54a9699878526fdd7a4dfd94ce17eb3e0a9
Andrew Robinson <aprobi@gmail.com>
2014-12-14 22:42:56 +0800

    Set all frameworks to .Net 4.0 and Re-Installed Nuget Packages

--- aaLogSplunkConsole/Program.cs
+++ aaLogSplunkConsole/Program.cs
@@ -11,21 +11,8 @@ namespace aaLogSplunkConsole
     {
         static void Main(string[] args)
         {
-            /*
-             * Arguments
-             * Filename             Override the path to the log file if specfied
-             * Maximum Messages     Override the default max messages to specify a larger or smaller value to return
-             */
-
-            List<LogRecord> logRecords = new List<LogRecord>();
-            aaLogReader.aaLogReader logReader;
-
-            //// All defaults
-            //if(args.Length == 0)
-            //{
-                logReader = new aaLogReader.aaLogReader();
-                logRecords = logReader.GetUnreadRecords();
-            //}
+            aaLogReader.aaLogReader logReader = new aaLogReader.aaLogReader();
+            List<LogRecord> logRecords = logReader.GetUnreadRecords();
 
             // If we have any records then output kvp format to the console
             if(logRecords.Count > 0)
