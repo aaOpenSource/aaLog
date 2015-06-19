e513715c949def3624db59836b5cea4b492e5977
arobinsongit <andy@phase2automation.com>
2015-06-19 19:58:41 +0800

    Forgot to include LogRecrordFilter type in previous commit.

---
+++ aaLogReader/Struct/LogRecordFilter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace aaLogReader
+{
+    public struct LogRecordFilter
+    {
+        public string Field;
+        public string Filter;
+    }
+}
