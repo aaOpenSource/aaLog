10c7c58c2f717d216ff132da51b79ec3591ce05a
Andrew Robinson <aprobi@gmail.com>
2015-11-08 17:14:15 -0500

    Cleaned up dead code and removed unnecessary files.

--- aaLogWebAPI/Program.cs
+++ aaLogWebAPI/Program.cs
@@ -1,22 +1,4 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
-
-//namespace aaLogWebAPI
-//{
-//    class Program
-//    {
-//        static void Main(string[] args)
-//        {
-//            Console.WriteLine("Hello world");
-//            Console.ReadLine();
-//        }
-//    }
-//}
-
-
+﻿
 using System;
 using System.Collections.Generic;
 using System.Linq;
