7470e6a39b3067471c5269a3c04667f0fbb387bf
Andy Robinson <aprobi@gmail.com>
2016-11-18 21:01:32 -0500

    Added Log Record Extensions

---
+++ aaLogReader/Helpers/LogRecordExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+using aaLogReader;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using System.Text;
+
+
+namespace aaLogReader.Helpers
+{
+    /// <summary>
+    /// Extension methods for reading different data types from streams.
+    /// </summary>ring
+    public static class LogRecordExtensions
+    {
+        public static string ToJSON(this List<LogRecord> records)
+        {
+            return JsonConvert.SerializeObject(records);
+        }
+
+        public static string ToKVP(this List<LogRecord> records)
+        {
+            StringBuilder returnValue;
+
+            returnValue = new StringBuilder();
+
+            foreach(LogRecord record in records)
+            {
+                returnValue.AppendLine(record.ToKVP());
+            }
+
+            return returnValue.ToString();
+        }
+    }
+}
